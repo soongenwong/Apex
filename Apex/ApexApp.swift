248787563ebbeaec422e6d8bcb3aa5cd331715dc
Soong En Wong <wsoongen2015@gmail.com>
2025-07-05 22:36:02 +0100

    Initial Commit

---
+++ Apex/ApexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexApp.swift
+//  Apex
+//
+//  Created by Soong En Wong on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ApexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApexTests/ApexTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApexTests.swift
+//  ApexTests
+//
+//  Created by Soong En Wong on 05/07/2025.
+//
+
+import Testing
+@testable import Apex
+
+struct ApexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
